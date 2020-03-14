0a8a8bdd70ffdd9567465f84ff813d3b2d080292
JenrruPro <50652828+JenrruPro@users.noreply.github.com>
2020-03-13 21:15:26 -0700

    Update EmpezarCliente.java

--- Cliente/src/EmpezarCliente.java
+++ Cliente/src/EmpezarCliente.java
@@ -5,7 +5,7 @@ public class EmpezarCliente {
 
     public static void main(String[] args) {
         Cliente cl;
-        cl = new Cliente("192.168.100.68");
+        cl = new Cliente("192.168.100.68"); //poner ip de la computador donde estara el servidor
         cl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         cl.iniciarCliente();
     }
